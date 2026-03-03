61eff48f6c7f43fc5b71e8541f357635feb9deb6
Jeon-ByeongUk <jeonbeyounguk@gmail.com>
2026-03-03 20:43:45 +0900

    초기화

---
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article; //Article 클래스 임포트 확인
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+        //1.DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        //2.레포지토리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+
+        return "";
+    }
+}
