5e1018c97c332294d06b48e18eb490a5baf9cd1a
psvm203 <psvm203@proton.me>
2026-03-08 11:22:26 +0900

    chore: add transactional annotation to service

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.CreateArticleRequest;
 import com.example.demo.entity.Article;
 import com.example.demo.repository.BoardRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BoardService {
@@ -13,6 +14,7 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    @Transactional
     public Article CreateArticle(CreateArticleRequest request) {
         Article article = Article.from(request);
         boardRepository.save(article);
